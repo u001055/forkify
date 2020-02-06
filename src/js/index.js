ff65e637159d0a8558ed6696fbd12579bb8905a7
Oleg <52830599+u001055@users.noreply.github.com>
2020-02-06 21:14:12 +0300

    fix problem with "npm run dev"

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-// Global app controller
+import Search from './models/Search';
+
+
+const state = {};
+
+const controlSearch = async () => {
+    const query = 'pizza';
+
+    if (query) {
+        state.search = new Search(query);
+
+        await state.search.getResults();
+
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
