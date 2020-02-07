0a52e7a2b2a87e1c361de800d84950981fec2e77
Oleg <52830599+u001055@users.noreply.github.com>
2020-02-07 20:21:06 +0300

    render search results

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,22 @@
 import Search from './models/Search';
-
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 const state = {};
 
 const controlSearch = async () => {
-    const query = 'pizza';
+    const query = searchView.getInput();
 
     if (query) {
         state.search = new Search(query);
 
         await state.search.getResults();
 
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
-})
+});
