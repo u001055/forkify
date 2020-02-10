c59f6e78c3fed9f3b325147a0591bf27b1977c0c
Oleg <52830599+u001055@users.noreply.github.com>
2020-02-10 19:15:31 +0300

    1,2,3 pages

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,6 +10,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 };
 
 const limitRecipeTitle = (title, limit = 17) => {
